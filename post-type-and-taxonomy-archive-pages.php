2e914e2c1d2965a6b1c59c0b37679475ef503721
Timo Hubois <info@pixelsaft.de>
2023-11-29 17:04:32 +0100

    feat(post-type-and-taxonomy-archive-pages.php): add custom autoloader if Composer is not available

--- post-type-and-taxonomy-archive-pages.php
+++ post-type-and-taxonomy-archive-pages.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Post Type Archive Pages and Permalink Settings
  * Plugin URI:        https://github.com/timohubois/post-type-and-taxonomy-archive-pages/
  * Description:       Set the archive for your custom post types to display it on a specific page and control the permalinks of custom post type single pages and custom taxonomies.
- * Version:           0.1
+ * Version:           1.0
  * Requires at least: 5.0
  * Requires PHP:      8.0
  * Author:            Timo Hubois
@@ -23,12 +23,34 @@ if (!defined('PTAPAPS_PLUGIN_FILE')) {
     define('PTAPAPS_PLUGIN_FILE', __FILE__);
 }
 
-if (file_exists(plugin_dir_path(APAPS_PLUGIN_FILE) . 'vendor/autoload.php')) {
-    require plugin_dir_path(APAPS_PLUGIN_FILE) . 'vendor/autoload.php';
+// Autoloader via Composer if available.
+if (file_exists(plugin_dir_path(PTAPAPS_PLUGIN_FILE) . 'vendor/autoload.php')) {
+    require plugin_dir_path(PTAPAPS_PLUGIN_FILE) . 'vendor/autoload.php';
+}
+
+// Custom autoloader if Composer is not available.
+if (!file_exists(plugin_dir_path(PTAPAPS_PLUGIN_FILE) . 'vendor/autoload.php')) {
+    spl_autoload_register(function ($className) {
+        $prefix = 'Ptapas\\';
+
+        $baseDir = plugin_dir_path(PTAPAPS_PLUGIN_FILE) . 'classes/';
+
+        $length = strlen($prefix);
+        if (strncmp($prefix, $className, $length) !== 0) {
+            return;
+        }
+
+        $relativeClass = substr($className, $length);
+        $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+
+        if (file_exists($file)) {
+            require $file;
+        }
+    });
 }
 
 if (!defined('APAPS_TEXT_DOMAIN')) {
-    define('APAPS_TEXT_DOMAIN', get_file_data(PTAPAPS_PLUGIN_FILE, [ 'TextDomain' => 'Text Domain'], 'plugin')['TextDomain']);
+    define('APAPS_TEXT_DOMAIN', get_file_data(PTAPAPS_PLUGIN_FILE, ['TextDomain' => 'Text Domain'], 'plugin')['TextDomain']);
 }
 
 load_plugin_textdomain(PTAPAPS_PLUGIN_FILE);
