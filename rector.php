dc509db22a04a5b49e53e9dc0dd7b153e92fc799
Timo Hubois <info@pixelsaft.de>
2024-07-10 21:49:36 +0200

    chore(rector): update to newest version

--- rector.php
+++ rector.php
@@ -3,41 +3,29 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
-use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
-use Rector\Arguments\Rector\MethodCall\RemoveMethodCallParamRector;
-use Rector\Arguments\Rector\ClassMethod\ReplaceArgumentDefaultValueRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->autoloadPaths([
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/',
+    ])
+    ->withSkip([
+        '*/vendor/*',
+        '*/node_modules/*',
+    ])
+    ->withAutoloadPaths([
         __DIR__ . '/vendor/squizlabs/php_codesniffer/autoload.php',
         __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php',
-    ]);
-
-    $rectorConfig->paths([
-        __DIR__ . '/',
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__ . '/vendor',
-        __DIR__ . '/node_modules',
-    ]);
-
-    $rectorConfig->sets([
-        SetList::PHP_80,
-        SetList::CODING_STYLE,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::INSTANCEOF,
-        SetList::NAMING,
-        SetList::TYPE_DECLARATION
-    ]);
-
-    $rectorConfig->skip([
-        CallableThisArrayToAnonymousFunctionRector::class,
-    ]);
-
-    $rectorConfig->rule(RemoveMethodCallParamRector::class);
-    $rectorConfig->rule(ReplaceArgumentDefaultValueRector::class);
-};
+    ])
+    ->withTypeCoverageLevel(45)
+    ->withDeadCodeLevel(44);
+    // ->withPreparedSets(
+    //     deadCode: true,
+    //     codeQuality: true,
+    //     typeDeclarations: true,
+    //     privatization: true,
+    //     naming: true,
+    //     instanceOf: true,
+    //     earlyReturn: true,
+    //     strictBooleans: true,
+    //     phpunitCodeQuality: true,
+    // );
