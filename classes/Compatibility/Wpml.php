89034057d072b49adce28d81956cc7dcf96ea2cb
Timo Hubois <info@pixelsaft.de>
2024-07-16 01:00:36 +0200

    feat(Compatibility): Add is_plugin_active check in Wpml and Yoast classes

--- classes/Compatibility/Wpml.php
+++ classes/Compatibility/Wpml.php
@@ -35,6 +35,10 @@ final class Wpml
 
     public static function isWpmlActive(): bool
     {
+        if (!function_exists('is_plugin_active')) {
+            include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+        }
+
         return is_plugin_active('sitepress-multilingual-cms/sitepress.php');
     }
 
